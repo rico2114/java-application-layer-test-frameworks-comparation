d5870778602f9c5cd2e6267d4a84e1cec10376dc
Juan Quiceno <juan.2114@hotmail.com>
2020-05-27 15:14:10 -0500

    Threadcount example added with invocation count so it can rerun the test in multiple threads.

--- movies-testng/src/test/java/com/movies/testng/unit/model/MovieModelTest.java
+++ movies-testng/src/test/java/com/movies/testng/unit/model/MovieModelTest.java
@@ -10,7 +10,7 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 @Test(groups = "model")
 public class MovieModelTest {
 
-    @Test(dataProvider = "generateParametrizationForTestModelIsConstructedCorrectly")
+    @Test(threadPoolSize = 3, dataProvider = "generateParametrizationForTestModelIsConstructedCorrectly", invocationCount = 5)
     public void testModelIsConstructedCorrectly(final String name, final String description, final int score, final String youtubeUrl) {
         final Movie movie = new Movie(name, description, score, youtubeUrl);
 
